de80180a90950c7de020d906119403007012684b
Chandler Ross Stone <chandler-stone@outlook.com>
2023-03-25 12:22:24 -0500

    Array with enterable elements and spin button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
 
 function App() {
+  let [items, setItems] = useState([]);
+  let [ourInput, setOurInput] = useState("");
+  let [output, setOutput] = useState("");
+  
+  function addToList(item) {
+    // Copy the array and add the item to the end
+    setItems([...items, item]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>
+        Enter an item <input onChange={(e) => setOurInput(e.target.value)}/>
+      </label>
+      <button onClick = {() => addToList(ourInput)}>Add to list</button>
+      <button onClick = {() => setOutput}>Spin!</button>
+      {items.map((item) => {
+        return <h2>{item}</h2>
+      })}
+      <h1>Your next event is:</h1>
+      {output.map((output) => {
+        return <h1>{output}</h1>
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
