4bc02d282bdb708c74e7eea755ef517f1e83803e
Muhammad Usama <89618204+usamasana@users.noreply.github.com>
2023-03-25 16:02:35 -0500

    Update App.jsx
    B1 to main

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,6 @@
+import react, {useState} from "react";
+import "./App.css";
 import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react'
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -23,44 +23,43 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
 function App() {
-  let [activities, setActivities] = useState([])
-  let [activityInput, setActivityInput] = useState("")
-  
-  //console.log(activityInput)
-  //test
+  const [spaceName, setSpaceName] = useState("");
+  const [isFormVisible, seIsFormVisible] =useState(false);
+  const [isFormVisible2, seIsFormVisible2] =useState(false);
+  function handleCreateClick () {
+    seIsFormVisible2(false);
+    seIsFormVisible(true);
+ }
 
-  function addActivity(activity) {
-    setActivities([...activities, activity])
-  }
-
-
-  function removeActivity(activity) {
-    if(activities.includes(activity)) {
-      let list = [...activities]
-      let ind = list.indexOf(activity)
-      list = activities.splice(ind, 1)
-      setActivities(activities)
-      console.log(" removed " + activity)
-      return
+  function handleCreateClick2 () {
+    seIsFormVisible(false);
+    seIsFormVisible2(true);
     }
-    console.log(activity + " not found")
+  function handleInputChange(event) {
+    setSpaceName(event.target.value);
   }
-
-
-  function listActivities() {
-    let list = [...activities]
-    const listActs =  list.map(activity => <div>{activity}</div>);
-    return(
-      { listActs }
-    )
-  }  
-
-
   return (
-    <div>
-      
+    <div className="app-container">
+      <h1 className="app-title"> Welcome to cure boredom App</h1>
+      <div className="button-container">
+        <button className="create-button" onClick={handleCreateClick}> Create Room</button>
+        <button className="join-button" onClick={handleCreateClick2}>Join Room</button>
+      </div>
+      {isFormVisible && (
+        <div className="form-container">
+          <input type= "text" placeholder="Enter your Name" value={spaceName} onChange = {handleInputChange}/>
+          <button className="create-button2"> GO </button>
+        </div>
+      )}
+      {isFormVisible2 && (
+        <div className="form-container">
+          <input type= "text" placeholder="Enter your Name"  onChange = {handleInputChange}/>
+          <input type= "text" placeholder="Enter Room ID"  onChange = {handleInputChange}/>
+          <div className="button-container"> <button className="join-button2"> GO</button> </div>
+        </div>
+      )}   
     </div>
-  )
-}
+  );
+ }
 
-export default App;
+ export default App;
