62038045f63587c1b2e783bacbb9a1cf559f331e
Muhammad Usama <89618204+usamasana@users.noreply.github.com>
2023-03-25 18:17:08 +0000

    added create and join forms

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,24 +3,41 @@ import "./App.css";
 
 
  function App() {
-  const [showForm, setSHowForm] = useState(false);
-   function handleCreateClick () {
-    setSHowForm(true);
-   }
+  const [spaceName, setSpaceName] = useState("");
+  const [isFormVisible, seIsFormVisible] =useState(false);
+  const [isFormVisible2, seIsFormVisible2] =useState(false);
+  function handleCreateClick () {
+    seIsFormVisible2(false);
+    seIsFormVisible(true);
+ }
+  function handleCreateClick2 () {
+    seIsFormVisible(false);
+    seIsFormVisible2(true);
+    }
+  function handleInputChange(event) {
+    setSpaceName(event.target.value);
+  }
+
   return (
     <div className="app-container">
       <h1 className="app-title"> Welcome to cure boredom App</h1>
-      {showForm ? (
+      <div className="button-container">
+        <button className="create-button" onClick={handleCreateClick}> Create Room</button>
+        <button className="join-button" onClick={handleCreateClick2}>Join Room</button>
+      </div>
+      {isFormVisible && (
         <div className="form-container">
-          <input type= "text" placeholder="Enter your Name" />
+          <input type= "text" placeholder="Enter your Name" value={spaceName} onChange = {handleInputChange}/>
           <button> Create Room</button>
         </div>
-      ):(
-        <div className="button-container">
-        <button className="create-button" onClick={handleCreateClick}> Create Room</button>
-        <button className="join-button">Join Room</button>
-      </div>
-      )}  
+      )}
+      {isFormVisible2 && (
+        <div className="form-container">
+          <input type= "text" placeholder="Enter your Name" value={spaceName} onChange = {handleInputChange}/>
+          <input type= "text" placeholder="Enter Room ID" value={spaceName} onChange = {handleInputChange}/>
+          <button> Join Room</button>
+        </div>
+      )}   
     </div>
   );
  }
