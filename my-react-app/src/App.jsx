ffbe77ea366e4401348e7f144ec74ac3610b876b
Chandler Ross Stone <chandler-stone@outlook.com>
2023-03-25 13:53:36 -0500

    spinning wheel with dynamic elements

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -2,33 +2,50 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import WheelComponent from "react-wheel-of-prizes";
+
 
 
 function App() {
   let [items, setItems] = useState([]);
   let [ourInput, setOurInput] = useState("");
   let [output, setOutput] = useState("");
+  let [segments, setSegments] = useState([]);
   
+  const segColors = ["#EE4040", "#F0CF50", "#815CD1", "#3DA5E0", "#34A24F"];
+  const onFinished = (winner) => {
+    console.log(winner);
+  };
   function addToList(item) {
     // Copy the array and add the item to the end
     setItems([...items, item]);
+    setSegments([...segments, item]);
   }
-
+  console.log(segments)
   return (
-    <div>
+    <><div>
       <label>
-        Enter an item <input onChange={(e) => setOurInput(e.target.value)}/>
+        Enter an item <input onChange={(e) => setOurInput(e.target.value)} />
       </label>
-      <button onClick = {() => addToList(ourInput)}>Add to list</button>
-      <button onClick = {() => setOutput}>Spin!</button>
-      {items.map((item) => {
-        return <h2>{item}</h2>
-      })}
+      <button onClick={() => addToList(ourInput)}>Add to list</button>
+      <button onClick={() => setOutput("theOutput")}>Spin!</button>
+      {segments.map((item) => <h2>{item}</h2>)}
       <h1>Your next event is:</h1>
-      {output.map((output) => {
-        return <h1>{output}</h1>
-      })}
-    </div>
+      <h1>{output}</h1>
+
+    </div><WheelComponent
+        key={Math.random()}
+        segments = {segments}
+        segColors = {segColors}
+        onFinished = {(winner) => onFinished(winner)}
+        primaryColor = "black"
+        contrastColor = "white"
+        buttonText = "Spin"
+        isOnlyOnce = {false}
+        size = {190}
+        upDuration = {500}
+        downDuration = {600}
+        fontFamily = "Arial" /></>
   )
 }
 
