e37eb733e9766b54928d40d112701f67576d82b5
earocorn <amavb789@gmail.com>
2023-03-25 20:11:00 -0500

    room creation implementation
    createRoom button works, interacting with Firebase

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,9 @@
 import {useState} from "react";
 import "./App.css";
-import logo from './logo.svg';
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { collection, addDoc, getDocs, getDoc, doc, setDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -21,31 +22,99 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
 
 function App() {
 
   let [activities, setActivities] = useState([])
   let [activityInput, setActivityInput] = useState("")
+  let [createErrorMessage, setCreateErrorMessage] = useState("")
+  let [joinErrorMessage, setJoinErrorMessage] = useState("")
+  let [roomID, setRoomID] = useState(Math.floor(Math.random()*90000) + 10000)
+
+  const startTime = Date.now()
+
+  const inputError = "Please enter a name!"
+  const roomExistsError = "Room already exists!"
   
   //console.log(activityInput)
   //test
-  const [spaceName, setSpaceName] = useState("");
+  const [createSpaceName, setCreateSpaceName] = useState("");
+  const [joinSpaceName, setJoinSpaceName] = useState("");
   const [isFormVisible, seIsFormVisible] =useState(false);
   const [isFormVisible2, seIsFormVisible2] =useState(false);
+
   function handleCreateClick () {
     seIsFormVisible2(false);
     seIsFormVisible(true);
- }
+  }
 
   function handleCreateClick2 () {
     seIsFormVisible(false);
     seIsFormVisible2(true);
-    }
-  function handleInputChange(event) {
-    setSpaceName(event.target.value);
   }
 
+  function handleJoinInputChange(event) {
+    setJoinSpaceName(event.target.value);
+    setJoinErrorMessage("");
+  }
 
+  function handleCreateInputChange(event) {
+    setCreateSpaceName(event.target.value);
+    setCreateErrorMessage("");
+  }
+
+  function handleInputIDChange(event) {
+    setRoomID(event.target.value);
+    setJoinErrorMessage("");
+  }
+
+  async function joinRoomClick() {
+    let name = joinSpaceName
+    //TODO:
+    //handle join room event
+  }
+
+  async function createRoomClick() {
+    let name = createSpaceName
+    if(name.length == 0) {
+        setCreateErrorMessage(inputError)
+        return
+    }
+    //reference rooms/(roomID) document
+    const roomRef = doc(db, "rooms", roomID.toString());
+    //get doc asynchronously
+    const snapshot = await getDoc(roomRef);
+    //check if roomID document in rooms exists
+    if(snapshot.exists()) {
+      setCreateErrorMessage(roomExistsError)
+      return
+    }
+    //set values in new roomID doc
+    await setDoc(doc(db, 'rooms', roomID.toString()), {
+      name: "Room " + roomID,
+      id: roomID,
+      creator: name
+    });
+    //add room creator to list of room members
+    await setDoc(doc(db, 'rooms/'+roomID.toString()+'/users', name), {
+      name: name
+    });
+    //add template of 6 activities in the roomID documeht in wheelactivities/activities document
+    await setDoc(doc(db, 'rooms/'+roomID.toString()+'/wheelActivities', "activities"), {
+      a1: "",
+      a2: "",
+      a3: "",
+      a4: "",
+      a5: "",
+      a6: ""
+    });
+    //TODO:
+    //include code to go to room webpage of roomID
+    console.log("Room " + roomID + " successfully created!")
+  }
+
+/*
   function listActivities() {
     let list = [...activities]
     const listActs =  list.map(activity => <div>{activity}</div>);
@@ -53,6 +122,7 @@ function App() {
       { listActs }
     )
   } 
+*/
 
   return (
     <div className="app-container">
@@ -63,15 +133,17 @@ function App() {
       </div>
       {isFormVisible && (
         <div className="form-container">
-          <input type= "text" placeholder="Enter your Name" value={spaceName} onChange = {handleInputChange}/>
-          <button className="create-button2"> GO </button>
+          <label>{createErrorMessage}</label>
+          <input type= "text" placeholder="Enter your Name" value={createSpaceName} onChange = {handleCreateInputChange}/>
+          <button className="create-button2" onClick={createRoomClick}> GO </button>
         </div>
       )}
       {isFormVisible2 && (
         <div className="form-container">
-          <input type= "text" placeholder="Enter your Name"  onChange = {handleInputChange}/>
-          <input type= "text" placeholder="Enter Room ID"  onChange = {handleInputChange}/>
-          <div className="button-container"> <button className="join-button2"> GO</button> </div>
+          <label>{joinErrorMessage}</label>
+          <input type= "text" placeholder="Enter your Name" value={joinSpaceName} onChange = {handleJoinInputChange}/>
+          <input type= "text" placeholder="Enter Room ID" value={roomID} onChange = {handleInputIDChange}/>
+          <div className="button-container"> <button className="join-button2" onClick={joinRoomClick}> GO</button> </div>
         </div>
       )}   
     </div>
